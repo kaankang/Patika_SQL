1924c633ed5b02ecc1b871c291c8ed78eb781a97
Kaan Kanığ <115084638+kaankang@users.noreply.github.com>
2023-09-29 23:16:12 +0300

    Create 11 - UNION, INTERSECT and EXCEPT.sql

---
+++ 11 - UNION, INTERSECT and EXCEPT.sql
@@ -0,0 +1,17 @@
+--11.1.actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+
+SELECT first_name FROM actor
+UNION
+SELECT first_name FROM customer;
+
+--11.2.actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+
+SELECT first_name FROM actor
+INTERSECT
+SELECT first_name FROM customer;
+
+--11.3.actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+
+SELECT first_name FROM actor
+EXCEPT
+SELECT first_name FROM customer;
